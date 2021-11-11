b8d65d314e146b69a486ea792693a01a2deef90e
elzup <guild0105@gmail.com>
2021-11-11 20:37:55 +0900

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,86 +1,12 @@
 import * as vscode from 'vscode'
+import Decorator from './decorator'
 
-// this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
-  console.log('decorator sample is activated')
-
-  let timeout: NodeJS.Timer | undefined = undefined
-
-  // create a decorator type that we use to decorate small numbers
-  const smallNumberDecorationType =
-    vscode.window.createTextEditorDecorationType({
-      borderWidth: '1px',
-      borderStyle: 'solid',
-      overviewRulerColor: 'blue',
-      overviewRulerLane: vscode.OverviewRulerLane.Right,
-      light: {
-        // this color will be used in light color themes
-        borderColor: 'darkblue',
-      },
-      dark: {
-        // this color will be used in dark color themes
-        borderColor: 'lightblue',
-      },
-    })
-
-  // create a decorator type that we use to decorate large numbers
-  const largeNumberDecorationType =
-    vscode.window.createTextEditorDecorationType({
-      cursor: 'crosshair',
-      // use a themable color. See package.json for the declaration and default values.
-      backgroundColor: { id: 'myextension.largeNumberBackground' },
-    })
-
-  let activeEditor = vscode.window.activeTextEditor
-
-  function updateDecorations() {
-    if (!activeEditor) {
-      return
-    }
-    const regEx = /\d+/g
-    const text = activeEditor.document.getText()
-    const smallNumbers: vscode.DecorationOptions[] = []
-    const largeNumbers: vscode.DecorationOptions[] = []
-    let match
-
-    while ((match = regEx.exec(text))) {
-      const startPos = activeEditor.document.positionAt(match.index)
-      const endPos = activeEditor.document.positionAt(
-        match.index + match[0].length
-      )
-      const decoration = {
-        range: new vscode.Range(startPos, endPos),
-        hoverMessage: 'Number **' + match[0] + '**',
-      }
-
-      if (match[0].length < 3) {
-        smallNumbers.push(decoration)
-      } else {
-        largeNumbers.push(decoration)
-      }
-    }
-    activeEditor.setDecorations(smallNumberDecorationType, smallNumbers)
-    activeEditor.setDecorations(largeNumberDecorationType, largeNumbers)
-  }
-
-  function triggerUpdateDecorations() {
-    if (timeout) {
-      clearTimeout(timeout)
-      timeout = undefined
-    }
-    timeout = setTimeout(updateDecorations, 500)
-  }
-
-  if (activeEditor) {
-    triggerUpdateDecorations()
-  }
+  let decorator = new Decorator()
 
   vscode.window.onDidChangeActiveTextEditor(
     (editor) => {
-      activeEditor = editor
-      if (editor) {
-        triggerUpdateDecorations()
-      }
+      decorator.changeActiveTextEditor(editor)
     },
     null,
     context.subscriptions
@@ -88,11 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
 
   vscode.workspace.onDidChangeTextDocument(
     (event) => {
-      if (activeEditor && event.document === activeEditor.document) {
-        triggerUpdateDecorations()
-      }
+      decorator.changeTextDocument(event)
     },
     null,
     context.subscriptions
   )
 }
+
+export function deactivate() {}
