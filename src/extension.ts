3e405f0264954ec3ee8cbd778e6c887cf81ffb1e
elzup <guild0105@gmail.com>
2021-11-12 00:00:49 +0900

    fix decorator

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,80 @@
 import * as vscode from 'vscode'
-import Decorator from './decorator'
 
 export function activate(context: vscode.ExtensionContext) {
-  let decorator = new Decorator()
+  let activeEditor = vscode.window.activeTextEditor
+
+  const jumpPointDecorator = {
+    decorator: vscode.window.createTextEditorDecorationType({
+      borderWidth: '1px',
+      borderStyle: 'solid',
+      overviewRulerColor: 'blue',
+      overviewRulerLane: vscode.OverviewRulerLane.Right,
+      light: {
+        // this color will be used in light color themes
+        borderColor: 'darkblue',
+      },
+      dark: {
+        // this color will be used in dark color themes
+        borderColor: 'lightblue',
+      },
+    }),
+  }
+  let timeout: NodeJS.Timer | undefined = undefined
 
   vscode.window.onDidChangeTextEditorSelection(
-    decorator.changeTextEditorSelection,
+    triggerUpdateDecorations,
     null,
     context.subscriptions
   )
-  // vscode.window.onDidChangeActiveTextEditor(
-  //   (editor) => {
-  //     decorator.changeActiveTextEditor(editor)
-  //   },
-  //   null,
-  //   context.subscriptions
-  // )
 
-  // vscode.workspace.onDidChangeTextDocument(
-  //   (event) => {
-  //     decorator.changeTextDocument(event)
-  //   },
-  //   null,
-  //   context.subscriptions
-  // )
+  function updateDecorations() {
+    if (!activeEditor) return
+
+    const anchor = activeEditor.selection.anchor
+    const upLine = anchor.line - 1
+    const dwLine = anchor.line + 1
+
+    const jumpPoints: vscode.DecorationOptions[] = []
+
+    jumpPoints.push({
+      range: new vscode.Range(
+        upLine,
+        anchor.character,
+        upLine,
+        anchor.character + 1
+      ),
+      hoverMessage: 'jump pos up',
+    })
+    jumpPoints.push({
+      range: new vscode.Range(
+        dwLine,
+        anchor.character,
+        dwLine,
+        anchor.character + 1
+      ),
+      hoverMessage: 'jump pos down',
+    })
+
+    activeEditor.setDecorations(jumpPointDecorator.decorator, jumpPoints)
+  }
+  function triggerUpdateDecorations() {
+    if (timeout) {
+      clearTimeout(timeout)
+      timeout = undefined
+    }
+    timeout = setTimeout(updateDecorations, 500)
+  }
+
+  vscode.window.onDidChangeActiveTextEditor(
+    (editor) => {
+      activeEditor = editor
+      if (editor) {
+        triggerUpdateDecorations()
+      }
+    },
+    null,
+    context.subscriptions
+  )
 }
 
 export function deactivate() {}
