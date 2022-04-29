cc11bef0eb5d14fd9bec20ce70ef844b61ef78b1
elzup <guild0105@gmail.com>
2022-04-29 12:52:10 +0900

    Revert "dynamic deco WIP"
    This reverts commit 24a95eace26b8f0c0abf492699545cf26474eea2.

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,30 +1,32 @@
 import * as vscode from 'vscode'
 
-export const makeJumpPointDecorator = (color: string) =>
-  vscode.window.createTextEditorDecorationType({
-    backgroundColor: color,
-    dark: {
-      borderColor: 'white',
-    },
-    // borderColor: { id: 'editorCursor.background' },
-    // not working
-    // borderColor: new vscode.ThemeColor('editorCursor.background')
-  })
+export const jumpPointDecorator = vscode.window.createTextEditorDecorationType({
+  backgroundColor: '#88888850',
+  dark: {
+    borderColor: 'white',
+  },
+  // borderColor: { id: 'editorCursor.background' },
+  // not working
+  // borderColor: new vscode.ThemeColor('editorCursor.background')
+})
 
-const borderDecoBase: vscode.DecorationRenderOptions = {
+export const jumpLineDecorator = vscode.window.createTextEditorDecorationType({
+  backgroundColor: '#44444440',
+})
+
+export const topBorderDeco = vscode.window.createTextEditorDecorationType({
   isWholeLine: true,
-}
-
-export const makeJumpLineDecorator = (
-  borderLigth: string,
-  borderDark: string,
-  top: boolean
-) =>
-  vscode.window.createTextEditorDecorationType({
-    ...borderDecoBase,
-    borderColor: borderLigth,
-    borderStyle: top ? 'solid none none none' : 'none none solid none',
-    dark: {
-      borderColor: borderDark,
-    },
-  })
+  borderStyle: 'solid none none none',
+  borderColor: '#00000020',
+  dark: {
+    borderColor: '#ffffff20',
+  },
+})
+export const btmBorderDeco = vscode.window.createTextEditorDecorationType({
+  isWholeLine: true,
+  borderStyle: 'none none solid none',
+  borderColor: '#00000020',
+  dark: {
+    borderColor: '#ffffff20',
+  },
+})
