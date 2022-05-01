4610e6c8837633711e70b2ef6c8983f0da497b7e
elzup <guild0105@gmail.com>
2022-05-01 21:46:17 +0900

    add test

--- src/test/logic.test.ts
+++ src/test/logic.test.ts
@@ -4,10 +4,55 @@ describe('calcDraw', () => {
   it('point mode', () => {
     const res = calcDraw(
       { distance: 5, mode: 'point', linePos: 'inside' },
-      { line: 4, character: 4 },
-      10
+      { line: 10, character: 4 },
+      20
     )
 
-    expect(res).toMatchInlineSnapshot()
+    expect(res.upDecoLines).toHaveLength(0)
+    expect(res.dwDecoLines).toHaveLength(0)
+    expect(res.nextLines).toStrictEqual([5, 15])
+  })
+
+  it('overflow line', () => {
+    const res = calcDraw(
+      { distance: 5, mode: 'point', linePos: 'inside' },
+      { line: 3, character: 4 },
+      20
+    )
+
+    expect(res.nextLines).toStrictEqual([0, 8])
+  })
+
+  it('line mode', () => {
+    const res = calcDraw(
+      { distance: 5, mode: 'line', linePos: 'inside' },
+      { line: 10, character: 4 },
+      20
+    )
+
+    expect(res.upDecoLines).toStrictEqual([15])
+    expect(res.dwDecoLines).toStrictEqual([5])
+  })
+
+  it('para mode', () => {
+    const res = calcDraw(
+      { distance: 5, mode: 'para', linePos: 'inside' },
+      { line: 8, character: 4 },
+      20
+    )
+
+    expect(res.upDecoLines).toStrictEqual([13, 18])
+    expect(res.dwDecoLines).toStrictEqual([3])
+  })
+
+  it('draw outside', () => {
+    const res = calcDraw(
+      { distance: 5, mode: 'para', linePos: 'outside' },
+      { line: 8, character: 4 },
+      20
+    )
+
+    expect(res.upDecoLines).toStrictEqual([3])
+    expect(res.dwDecoLines).toStrictEqual([13, 18])
   })
 })
